293b2fdf7b390b27907002f2de3686575ac391af
lauracazorla <lauracaz1@hotmail.com>
2023-05-26 19:05:07 +0200

    Added details to work in production

--- angular-loganalytics/src/app/config/constants.ts
+++ angular-loganalytics/src/app/config/constants.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core'
 @Injectable()
 export class Constants {
     public static API_ENDPOINT: string = 'http://localhost:8080/api/';
+    //public static API_ENDPOINT: string = 'http://localhost:8090/api/';
     public static COLORS: string[] = ["#33A8C7", "#52E3E1", "#A0E426", "#FDF148", "#FFAB00", 
     "#F77976", "#F050AE", "#D883FF", "#9336FD", "#FB5607"];
 }
