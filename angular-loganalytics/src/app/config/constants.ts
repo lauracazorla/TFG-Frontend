65e264a6c59ecc338fa96322e8d0efd85ed8ce26
lauracazorla <lauracaz1@hotmail.com>
2023-06-07 16:15:13 +0200

    API url settings for different cases

--- angular-loganalytics/src/app/config/constants.ts
+++ angular-loganalytics/src/app/config/constants.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@angular/core'
 
 @Injectable()
 export class Constants {
-    public static API_ENDPOINT: string = 'http://localhost:8080/api/';
+    //bootRun + ng serve
+    //public static API_ENDPOINT: string = 'http://localhost:8080/api/';
+    //Docker deployed in local machine
     //public static API_ENDPOINT: string = 'http://localhost:8090/api/';
+    //Docker deployed in remote server
+    public static API_ENDPOINT: string = 'http://gessi-dashboard.essi.upc.edu:8090/api/';
     public static COLORS: string[] = ["#33A8C7", "#52E3E1", "#A0E426", "#FDF148", "#FFAB00", 
     "#F77976", "#F050AE", "#D883FF", "#9336FD", "#FB5607"];
 }
